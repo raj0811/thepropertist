690b6302eeced409fab56a544fe9d4cf7e43bf48
raj0811 <rpbarmaiya@gmail.com>
2026-07-19 11:36:43 +0530

    add vercel url in whitelist

--- dist/main.js
+++ dist/main.js
@@ -8,6 +8,7 @@ async function bootstrap() {
         origin: [
             "http://localhost:5173",
             "http://localhost:5174",
+            "https://thepropertist-frontend.vercel.app",
             process.env.FRONTEND_URL,
         ].filter(Boolean),
         credentials: true,
